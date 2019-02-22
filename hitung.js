2cd3e107c153d1bcc253ddf13f26cf57c31857a7
syukurd <47785145+syukurd@users.noreply.github.com>
2019-02-22 10:53:32 +0700

    Update hitung.js
    final js

--- hitung.js
+++ hitung.js
@@ -1,24 +1,30 @@
-function tambah(){
-   var angkasatu= parseInt(document.getElementById("angka1").value);
-   var angkadua = parseInt(document.getElementById("angka2").value);
-    var hasil = angkasatu + angkadua;
-    alert("Hasil dari Penjumlahan " +hasil);
-}
-function kurang(){
-    var angkasatu= parseInt(document.getElementById("angka1").value);
+function tambah() {
+    var angkasatu = parseInt(document.getElementById("angka1").value);
     var angkadua = parseInt(document.getElementById("angka2").value);
-     var hasil = angkasatu - angkadua;
-     alert(hasil);
- }
- function bagi(){
-    var angkasatu= parseFloat(document.getElementById("angka1").value);
+    var hasil = angkasatu + angkadua;
+    document.getElementById("hasilnya").innerHTML = hasil;
+}
+function kurang() {
+    var angkasatu = parseInt(document.getElementById("angka1").value);
+    var angkadua = parseInt(document.getElementById("angka2").value);
+    var hasil = angkasatu - angkadua;
+    document.getElementById("hasilnya").innerHTML = hasil;
+}
+function bagi() {
+    var angkasatu = parseFloat(document.getElementById("angka1").value);
     var angkadua = parseFloat(document.getElementById("angka2").value);
-     var hasil = angkasatu / angkadua;
-     alert(hasil);
- }
- function kali(){
-    var angkasatu= parseFloat(document.getElementById("angka1").value);
+    var hasil = angkasatu / angkadua;
+    document.getElementById("hasilnya").innerHTML = hasil;
+}
+function kali() {
+    var angkasatu = parseFloat(document.getElementById("angka1").value);
     var angkadua = parseFloat(document.getElementById("angka2").value);
-     var hasil = angkasatu * angkadua;
-     alert(hasil);
- }
+    var hasil = angkasatu * angkadua;
+    document.getElementById("hasilnya").innerHTML = hasil;
+
+
+
+}
+function hasil() {
+    location.reload();
+}
